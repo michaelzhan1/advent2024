24796c5c8bf95fb3dd1a3224fe93abcc8000032a
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-09 01:35:57 -0500

    Day 9

---
+++ day09.py
@@ -0,0 +1,95 @@
+def parse():
+    with open('day09.in') as f:
+        return f.read().strip()
+
+# TODO: look at 2 digit values?
+def compact(line):
+    # even = ID, odd = blank
+    res = [""] * sum(int(x) for x in line)
+    res_i = 0
+    for i in range(len(line)):
+        if i % 2 == 0:
+            for _ in range(int(line[i])):
+                res[res_i] = str(i // 2)
+                res_i += 1
+        else:
+            res_i += int(line[i])
+
+    i = 0
+    j = len(res) - 1
+    while i < j:
+        if res[i] != '':
+            i += 1
+        elif res[j] == '':
+            j -= 1
+        else:
+            res[i], res[j] = res[j], res[i]
+            i += 1
+            j -= 1
+
+    final_idx = 0
+    while res[final_idx] != '':
+        final_idx += 1
+    return res[:final_idx]
+
+def get_checksum(line):
+    compacted = compact(line)
+    return sum([int(compacted[i]) * i for i in range(len(compacted))])
+
+def compact_blocks(line):
+    # file: (id, count)
+    # gap: (filled bool, count, filled id)
+    memory = []
+    for i in range(len(line)):
+        if i % 2 == 0:
+            memory.append((str(i // 2), int(line[i])))
+        else:
+            memory.append((False, int(line[i]), None))
+    
+    # TODO: also let remaining gap be a thing
+    i = len(memory) - 1
+    while i >= 0:
+        if len(memory[i]) == 2: # file
+            for j in range(i):
+                # if there's a gap that can fit the file, we need to:
+                # 1. fill in the correct amount in the gap
+                # 2. turn the existing file space into a gap
+                # 3. recreate the remaining gap (and update i accordingly)
+                if len(memory[j]) == 3 and not memory[j][0] and memory[j][1] >= memory[i][1]:
+                    remaining = memory[j][1] - memory[i][1]
+                    memory[j] = (True, memory[i][1], memory[i][0])
+                    memory[i] = (False, memory[i][1], None)
+                    if remaining > 0:
+                        memory.insert(j + 1, (False, remaining, None))
+                        i += 1
+                    break
+        i -= 1
+
+    # recreate the memory
+    arr = ''
+    for cell in memory:
+        if len(cell) == 2:
+            arr += cell[0] * cell[1]
+        elif cell[0]:
+            arr += cell[2] * cell[1]
+        elif not cell[0]:
+            arr += '0' * cell[1]
+    return arr
+
+def get_checksum_blocks(line):
+    compacted = compact_blocks(line)
+    return sum([int(compacted[i]) * i for i in range(len(compacted))])
+
+def main():
+    line = parse()
+    res = get_checksum(line)
+    print(f"Part 1: {res}")
+
+    res = get_checksum_blocks(line)
+    print(f"Part 2: {res}")
+    
+
+
+
+if __name__ == "__main__":
+    main()
