9e678ddc616779c33624fe9b135da531126e9859
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-23 18:29:37 -0500

    Day 21 part 2

--- day21.py
+++ day21.py
@@ -1,81 +1,96 @@
+from functools import cache
+
 def parse():
     with open('day21.in') as f:
         return f.read().splitlines()
-    
-def get_key_presses(sequence, keypad_type):
-    if keypad_type == 1:
-        keypad = ['789', '456', '123', '.0A']
+
+def generate_paths():
+    paths = {
+        'digits': {},
+        'directions': {}
+    }
+
+    kps = [
+        ['789', '456', '123', '.0A'],
+        ['.^A', '<v>']
+    ]
+
+    names = ['digits', 'directions']
+
+    for i in range(2):
+        keypad = kps[i]
         n = len(keypad)
         m = len(keypad[0])
-    else:
-        keypad = ['.^A', '<v>']
-        n = len(keypad)
-        m = len(keypad[0])
+        positions = {keypad[i][j]: (i, j) for i in range(n) for j in range(m)}
 
-    positions = {keypad[i][j]: (i, j) for i in range(n) for j in range(m)}
-    i, j = positions['A']
-    gap_i, gap_j = positions['.']
+        gap_i, gap_j = positions['.']
+        
+        for key1 in positions:
+            for key2 in positions:
+                if key1 == '.' or key2 == '.':
+                    continue
 
-    move = ''
-    for char in sequence:
-        new_i, new_j = positions[char]
-        i_char = 'v' if new_i > i else '^' if new_i < i else ''
-        j_char = '>' if new_j > j else '<' if new_j < j else ''
+                i1, j1 = positions[key1]
+                i2, j2 = positions[key2]
 
-        di = abs(new_i - i)
-        dj = abs(new_j - j)
+                di = abs(i2 - i1)
+                dj = abs(j2 - j1)
 
-        if (i == gap_i and new_j == gap_j) or (new_i == gap_i and j == gap_j):
-            # horizontal first
-            if i == gap_i and new_j == gap_j:
-                move += i_char * di + j_char * dj + 'A'
-            else:
-                move += j_char * dj + i_char * di + 'A'
+                i_char = 'v' if i2 > i1 else '^' if i2 < i1 else ''
+                j_char = '>' if j2 > j1 else '<' if j2 < j1 else ''
+
+
+                if i1 == i2 and j1 == j2:
+                    move = 'A'
+                elif (i1 == gap_i and j2 == gap_j) or (i2 == gap_i and j1 == gap_j):
+                    if i1 == gap_i and j2 == gap_j:
+                        move = i_char * di + j_char * dj + 'A'
+                    else:
+                        move = j_char * dj + i_char * di + 'A'
+                else:
+                    if (i2 < i1 and j2 > j1) or (i2 > i1 and j2 > j1):
+                        move = di * i_char + dj * j_char + 'A'
+                    else:
+                        move = dj * j_char + di * i_char + 'A'
+
+                paths[names[i]][(key1, key2)] = move
+    return paths
+
+def get_final_length(code, times):
+    all_paths = generate_paths()
+
+    @cache
+    def count(code, remaining):
+        if remaining == times: # first time
+            paths = all_paths['digits']
         else:
-            # up right or down right: press vertical first
-            if (new_i < i and new_j > j) or (new_i > i and new_j > j):
-                move += di * i_char + dj * j_char + 'A'
-            else:
-                move += dj * j_char + di * i_char + 'A'
+            paths = all_paths['directions']
 
-        i, j = new_i, new_j
-
-    return move
-
-def get_complexity(original_code):
-    code1 = get_key_presses(original_code, 1)
-    code2 = get_key_presses(code1, 2)
-    code3 = get_key_presses(code2, 2)
-    return len(code3) * int(original_code[:-1])
+        if remaining == 0:
+            return len(code)
+        
+        prev = 'A'
+        res = 0
+        for char in code:
+            res += count(paths[(prev, char)], remaining - 1)
+            prev = char
+        return res
     
-def get_complexities(codes):
+    return count(code, times)
+
+def get_total_complexity(codes, count):
     res = 0
     for code in codes:
-        res += get_complexity(code)
+        res += get_final_length(code, count) * int(code[:-1])
     return res
-
-def get_complexity_25(original_code):
-    code = get_key_presses(original_code, 1)
-    for _ in range(25):
-        code = get_key_presses(code, 2)
-    return len(code) * int(original_code[:-1])
-
-def get_complexities_25(codes):
-    res = 0
-    for code in codes:
-        print(code)
-        res += get_complexity_25(code)
-    return res
-
+        
 def main():
     codes = parse()
-    res = get_complexities(codes)
+    res = get_total_complexity(codes, 3)
     print(f"Part 1: {res}")
 
-    res = get_complexities_25(codes)
+    res = get_total_complexity(codes, 26)
     print(f"Part 2: {res}")
-
-
 
 if __name__ == "__main__":
     main()
